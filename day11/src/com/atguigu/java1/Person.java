bf2e417b071bf899dcde12dbcc362212c742d05d
kuikui1998 <shakuil@qq.com>
2020-06-06 14:43:05 +0800

    反射机制

--- day11/src/com/atguigu/java1/Person.java
+++ day11/src/com/atguigu/java1/Person.java
@@ -41,4 +41,17 @@ public class Person extends Creature<String> implements Comparable<String>,MyInt
     public int compareTo(String o) {
         return 0;
     }
+
+    private static void showDesc(){
+        System.out.println("我是一个不同的人");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                '}';
+    }
 }
